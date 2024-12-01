ab0a7f74a3f8e9aa8d394ad4af912f47099ad8ac
mraji <mraji@DEV-MRAJI>
2024-12-01 12:16:07 +0330

    https://www.youtube.com/watch?v=BSC1KJUfqCM

--- MinimalApiTutorial/Program.cs
+++ MinimalApiTutorial/Program.cs
@@ -2,6 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "hello world");
+
+app.MapGet("/hello-get", () => "[GET]hello world");
+app.MapPost("/hello-post", () => "[POST]hello world");
+app.MapPut("/hello-put", () => "[PUT]hello world");
+app.MapDelete("/hello-delete", () => "[DELETE]hello world");
+
+app.MapMethods("/new-hello-get", new[] { HttpMethods.Get }, () => "[GET] New Hello World!");
+app.MapMethods("/hello-patch", new[] { HttpMethods.Patch }, () => "[PATCH] New Hello World!");
 
 app.Run();
