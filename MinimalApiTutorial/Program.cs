a20efe3627066e97ff98bee533e60836745dbed3
Mehdi Raji <148987332+mehdi-raji@users.noreply.github.com>
2024-12-06 20:48:25 +0330

    Get Parameter https://www.youtube.com/watch?v=AwJ4gScxOsA

--- MinimalApiTutorial/Program.cs
+++ MinimalApiTutorial/Program.cs
@@ -15,55 +15,6 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-//
-// app.MapGet("/hello", () => "hello with name")
-// 	.WithName("hi");
-//
-// app.MapGet("/url", (LinkGenerator link) =>
-// 	$"{link.GetPathByName("hi", values: null)}");
-RouteBuilderExtensions.Map(app);
 
+app.MapGet("/student/{name}/{id:int}", (string name,int id) => $"Hello {name}");
 app.Run();
-
-public static class RouteBuilderExtensions
-{
-	public static void Map(WebApplication app)
-	{
-		// Map endpoints with IResult handlers
-		app.MapGet("/students", GetAllStudents)
-			.WithName("GetAllStudents") 
-			.WithTags("Student API");   
-
-		app.MapPost("/students", AddStudent)
-			.WithName("AddStudent")     
-			.WithTags("Student API");
-
-		app.MapPut("/students", EditStudent)
-			.WithName("EditStudent")    
-			.WithTags("Student API");
-
-		app.MapDelete("/students", DeleteStudent)
-			.WithName("DeleteStudent")  
-			.WithTags("Student API");
-	}
-
-	private static IResult GetAllStudents()
-	{
-		return Results.Ok(new { Message = "All Students" });
-	}
-
-	private static IResult AddStudent()
-	{
-		return Results.Ok(new { Message = "Insert Student" });
-	}
-
-	private static IResult EditStudent()
-	{
-		return Results.Ok(new { Message = "Edit Student" });
-	}
-
-	private static IResult DeleteStudent()
-	{
-		return Results.Ok(new { Message = "Delete Student" });
-	}
-}
