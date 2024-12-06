e7ebc411e29f1f4d130dfa95f02e2f823a37b2f6
Mehdi Raji <148987332+mehdi-raji@users.noreply.github.com>
2024-12-06 21:18:49 +0330

    Response Type https://www.youtube.com/watch?v=aoKmNpf1CuE

--- MinimalApiTutorial/Program.cs
+++ MinimalApiTutorial/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,5 +17,30 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.MapGet("/student/{name}/{id:int}", (string name,int id) => $"Hello {name}");
+var people = new List<Person>
+{
+	new() { Id = 1, Name = "Reza" },
+	new() { Id = 2, Name = "Ali" },
+};
+app.MapGet("results/person/{id:int}", (int id) =>
+{
+	var person = people.FirstOrDefault(x => x.Id == id);
+
+	return person is null ? Results.NotFound() : Results.Ok(person);
+});
+
+app.MapGet("typedresult/person/{id:int}", Results<Ok<Person>,NotFound>(int id) =>
+{
+	var person = people.FirstOrDefault(x => x.Id == id);
+
+	return person is null ? TypedResults.NotFound() : TypedResults.Ok(person);
+});
+
+app.MapGet("/student/{name}/{id:int}", (string name, int id) => $"Hello {name}");
 app.Run();
+
+public class Person
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+}
